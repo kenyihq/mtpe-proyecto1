8e4d0cce3af7a6556c00fe0ca19ce70c395153ad
IsmaelQR <ismael.quispe.ra@gmail.com>
2022-11-06 16:33:29 -0500

    Busqueda por ISBN y Titulo

--- Tarea_1/main.py
+++ Tarea_1/main.py
@@ -22,6 +22,16 @@ def listar(datos):
         i.listar()
 
 
+def agregar():
+    idlibro = input('id:')
+    titulo = input('titulo:')
+    genero = input('genero:')
+    isbn = input('isbn:')
+    editorial = input('editorial:')
+    autor = input('autor :')
+    x = Libro(idlibro, titulo, genero, isbn, editorial, autor)
+
+
 class Libro:
     def __init__(self, idlibro, titulo, genero, isbn, editorial, autor):
         self.idlibro = idlibro
@@ -35,17 +45,56 @@ class Libro:
         print(f'El libro {self.titulo} de {self.autor} del genero {self.genero} posee el ISBN: {self.isbn}')
 
 
-    def agregar(self):
-        idlibro = input('id:')
-        titulo = input('titulo:')
-        genero = input('genero:')
-        isbn = input('isbn:')
-        editorial = input('editorial:')
-        autor = input('autor :')
-        x=Libro(idlibro,titulo,genero,isbn,editorial,autor)
+def searchisbntitle(datos: list):
+    print('Escriba el numero opcion que requiera')
+    print('Opcion 1 : Buscar por ISBN')
+    print('Opcion 2 : BUscar por Titulo')
+    option = 0
+    while (option != 1) and (option != 2):
+        option = int(input("Seleccione una opcion: "))
+    if option == 1:
+        isbn = input('Escribir ISBN : ')
+        flag = True
+        for n in datos:
+            if n.isbn == isbn:
+                n.listar()
+                flag = False
+        if flag:
+            print('No existe un valor con el ISBN solicitado')
+    elif option == 2:
+        title = input('Escribir Titulo : ')
+        flag = True
+        for n in datos:
+            if n.titulo == title:
+                n.listar()
+                flag = False
+        if flag:
+            print('El valor no se encuentra ')
+
+
+def ordertitle(datos: list):
+    new = []
+    back = datos
+    tit_minimo = 'zzzzz'
+    lib_minimo = None
+    # Calculamos el minimo
+    for y in range(len(datos)):
+        for x in back:
+            if x.titulo < tit_minimo:
+                lib_minimo = x
+                tit_minimo = x.titulo
+        new.append(lib_minimo)
+        back.remove(lib_minimo)
+        print(new[0].titulo)
+        print(back[0].titulo)
+
+    # Ordenamos a partir del minimo
+    # for x in back:
 
 
 # libro1 = Libro(1, 'The book', 'Accion', 'ISBN', 'El comercio', 'Anonimo')
 # readbook(datos)
 readbooks(biblio)
-listar(biblio)
+# listar(biblio)
+# searchisbntitle(biblio)
+ordertitle(biblio)
